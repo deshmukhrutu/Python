b09b453a6a30f61728df5c797ba14eec24340612
deshmukhrutu <127130315+deshmukhrutu@users.noreply.github.com>
2023-03-10 08:07:18 -0800

    Add files via upload

---
+++ Reverse_String.py
@@ -0,0 +1,7 @@
+str=input("enter string   ")
+""" In the slicing notation the first value(:) represents the 
+starting index(inclusive) and second value represents(:) represents the ending index(exclusive)
+and the third value (-1) represents the step by using step of -1,the string is reversed
+"""
+reverse_string=str[::-1]
+print(reverse_string)
